d4ef139807695a60225e430257d1c68fb5bd1c03
Derecho <derecho@sector5d.org>
2016-06-14 20:50:00 +0200

    Added include guard

--- ad9850/ad9850.h
+++ ad9850/ad9850.h
@@ -1,6 +1,11 @@
+#ifndef AD9850_H
+#define AD9850_H
+
 #include <stdint.h>
 
 void dds_setup(void);
 void dds_reset(void);
 void dds_write(uint8_t byte);
 void dds_update_freq(float freq);
+
+#endif  // AD9850_H
